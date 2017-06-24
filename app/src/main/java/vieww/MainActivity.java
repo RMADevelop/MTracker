a41733385c0387820f0a7f866a2586d4d92fca45
RomanM <arct1k77@gmail.com>
2017-06-24 14:33:38 +0300

    delete tabLayer

--- app/src/main/java/vieww/MainActivity.java
+++ app/src/main/java/vieww/MainActivity.java
@@ -23,12 +23,8 @@ import com.example.roma.mtracker_v3.R;
 import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    private ViewPager mViewPager;
-    private PagerSlidingTabStrip tabs;
+    FragmentManager fm;
+    Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,46 +36,20 @@ public class MainActivity extends AppCompatActivity {
 
         Realm.init(this);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
 
 
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-        tabs.setViewPager(mViewPager);
+        fm = getSupportFragmentManager();
+        mFragment = fm.findFragmentById(R.id.container);
+        if (mFragment == null) {
+            mFragment = new FragmentMonthThis();
+            fm.beginTransaction()
+                    .add(R.id.container, mFragment)
+                    .commit();
+        }
 
 
     }
 
-
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -106,41 +76,5 @@ public class MainActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 1) {
-                return new FragmentMonthThis();
-            } else {
-                return new FragmentMonthThis();
-            }
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-//            return FragmentInsert.newInstance();
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 1;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
